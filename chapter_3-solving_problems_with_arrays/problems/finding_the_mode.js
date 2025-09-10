3ec5cdbab52aa57e1edd1df15a2e26d9ad69094c
oktaygokhanboz <contact@oktaygokhanboz.com>
2025-09-10 14:03:28 +0300

    refactor: store survey values in array as a histogram

--- chapter_3-solving_problems_with_arrays/problems/finding_the_mode.js
+++ chapter_3-solving_problems_with_arrays/problems/finding_the_mode.js
@@ -1,22 +1,24 @@
 const surveyResults = [4, 7, 3, 8, 9, 7, 3, 9, 9, 3, 3, 10];
 
 function findMode(array) {
-  let modelIndex;
-  let modelCount = 0;
-  let count;
+  const histogram = Array(10).fill(0);
 
-  array.forEach((e, i) => {
-    count = 0;
-    for (const element of array) {
-      if (e === element) count++;
-    }
-    if (count > modelCount) {
-      modelCount = count;
-      modelIndex = i;
-    }
-  });
+  // store count of the same values
+  for (let i = 0; i < surveyResults.length; i++) {
+    histogram[surveyResults[i] - 1]++;
+  }
 
-  return array[modelIndex];
+  // find highest value of histogram, in other words, mode of survey
+  let mode = histogram[0];
+  let modeIndex = 0;
+  for (let i = 1; i < histogram.length; i++) {
+    if (histogram[i] > mode) {
+      mode = histogram[i];
+      modeIndex = i;
+    }
+  }
+
+  return ++modeIndex;
 }
 
 console.log("The mode is " + findMode(surveyResults));
