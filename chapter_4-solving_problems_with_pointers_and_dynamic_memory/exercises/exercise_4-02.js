345919664065d53e47d27aaeb5a3c1028f019be4
oktaygokhanboz <contact@oktaygokhanboz.com>
2025-10-09 11:16:51 +0300

    Add special case handler to exercise 4.2

--- chapter_4-solving_problems_with_pointers_and_dynamic_memory/exercises/exercise_4-02.js
+++ chapter_4-solving_problems_with_pointers_and_dynamic_memory/exercises/exercise_4-02.js
@@ -3,6 +3,11 @@
 const text = "abcdefg";
 
 function substring(arrayString, startPosition, length) {
+  // special case
+  if (arrayString.at(startPosition - 1) === undefined) {
+    return "Position not found";
+  }
+
   let output = "";
   let count = 0;
   for (let i = 0; i < arrayString.length; i++) {
