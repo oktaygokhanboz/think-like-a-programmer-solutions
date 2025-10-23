1e1aadefffb8fd7ff09272420141eea043835610
oktaygokhanboz <contact@oktaygokhanboz.com>
2025-10-23 10:41:13 +0300

    Add exercise 4.7 solution

---
+++ chapter_4-solving_problems_with_pointers_and_dynamic_memory/exercises/exercise_4-07.js
@@ -0,0 +1,9 @@
+let text = "apple";
+
+function concatenate(text_1, text_2) {
+  text = text_1 + text_2;
+  return text;
+}
+
+console.log(concatenate(text, "red"));
+console.log(concatenate(text, "phone"));
