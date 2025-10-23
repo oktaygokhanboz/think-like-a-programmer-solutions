d2ad25a1deeace27b21ccdcffd3642ae7c746e20
oktaygokhanboz <contact@oktaygokhanboz.com>
2025-10-23 11:25:36 +0300

    Add special case handler to exercise 4.9

--- chapter_4-solving_problems_with_pointers_and_dynamic_memory/exercises/exercise_4-09.js
+++ chapter_4-solving_problems_with_pointers_and_dynamic_memory/exercises/exercise_4-09.js
@@ -1,9 +1,14 @@
-const input = [1, 4, 9];
+const input = [1, 4, 99];
 
 function intToList(numberList) {
   let stringNumber = "";
 
   for (const num of numberList) {
+    // special case: check numbers are digit
+    if (num < 0 || num > 9) {
+      return "input contains non-digit element";
+    }
+
     stringNumber += num.toString();
   }
   return parseInt(stringNumber);
